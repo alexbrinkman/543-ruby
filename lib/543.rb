7a420e0937ad951e43f9d90a624e02bfaae76502
Alex Brinkman <abrink@commercekitchen.com>
2017-11-08 17:02:09 -0700

    [CHG] Impl game management logic
    The game can now be played, though a bit rough.  All of the validation
    around valid moves and determining if the game has been won has been
    implemented, along with the abilty to make a move from the command
    line.

--- lib/543.rb
+++ lib/543.rb
@@ -1,7 +1,22 @@
 require_relative "board"
 
+require "pry"
+
 module Game543
   puts "Starting up"
   board = Board.new
   puts board
+  loop do
+    puts "Your move: (row)"
+    row = gets.chomp
+    puts "Your move: (num)"
+    num = gets.chomp
+    begin
+      status = board.move(row, num)
+    rescue ArgumentError => e
+      puts e.message
+    end
+    puts board
+    break if status == :winner
+  end
 end
--- lib/board.rb
+++ lib/board.rb
@@ -3,10 +3,23 @@ module Game543
   class Board
 
     def initialize
-      @row1 = [true, true, true]
-      @row2 = [true, true, true, true]
-      @row3 = [true, true, true, true, true]
-      @position = [@row1, @row2, @row3]
+      @position = [
+        [true, true, true],
+        [true, true, true, true],
+        [true, true, true, true, true]
+      ]
+    end
+
+    def move(row, num)
+      row = row.to_i
+      num = num.to_i
+      validate_move(row, num)
+      update_board(row, num)
+      if winner?
+        puts "WINNER!"
+        return :winner
+      end
+      :ok
     end
 
     def to_s
@@ -16,6 +29,28 @@ module Game543
         puts "\n"
       end
       puts "---------"
+    end
+
+    private
+
+    def validate_move(row, num)
+      raise ArgumentError.new("Invalid row: please enter 1, 2, or 3")  if row < 1 || row > 3
+      raise ArgumentError.new("Invalid num: please enter at least 1 piece.") if num < 1
+      raise ArgumentError.new("Invalid num: please enter no more than the number remaining in the row") if num > pieces_left(row)
+    end
+
+    def pieces_left(row)
+      @position[row - 1].select { |e| e }.size
+    end
+
+    def update_board(row, num)
+      new_row = []
+      (pieces_left(row) - num).times { new_row << true }
+      @position[row - 1] = new_row
+    end
+
+    def winner?
+      @position.flatten.count == 1
     end
 
   end
