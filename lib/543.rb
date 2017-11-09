02346b0e1a14e63d27941d8ddcd21c1575390964
Alex Brinkman <abrink@commercekitchen.com>
2017-11-09 10:11:23 -0700

    [CHG] Clean up and refactor base version
    * Refactor the basic existing code a bit, just cleaning up method
    calls, etc.
    * Add in check so that the user doesn't take all remaining peices,
    as that isn't allowed.

--- lib/543.rb
+++ lib/543.rb
@@ -1,22 +1,25 @@
 require_relative "board"
-
 require "pry"
 
-module Game543
-  puts "Starting up"
-  board = Board.new
-  puts board
-  loop do
-    puts "Your move: (row)"
-    row = gets.chomp
-    puts "Your move: (num)"
-    num = gets.chomp
-    begin
-      status = board.move(row, num)
-    rescue ArgumentError => e
-      puts e.message
-    end
-    puts board
-    break if status == :winner
+board = Game543::Board.new
+puts board
+
+loop do
+  puts "Your move: (row)"
+  row = gets.chomp
+  puts "Your move: (num)"
+  num = gets.chomp
+
+  begin
+    status = board.move(row, num)
+  rescue ArgumentError => e
+    puts e.message
   end
+  puts board
+
+  if board.winner?
+    puts "WINNER!"
+    exit
+  end
+  
 end
--- lib/board.rb
+++ lib/board.rb
@@ -15,11 +15,10 @@ module Game543
       num = num.to_i
       validate_move(row, num)
       update_board(row, num)
-      if winner?
-        puts "WINNER!"
-        return :winner
-      end
-      :ok
+    end
+
+    def winner?
+      total_pieces_left == 1
     end
 
     def to_s
@@ -35,22 +34,23 @@ module Game543
 
     def validate_move(row, num)
       raise ArgumentError.new("Invalid row: please enter 1, 2, or 3")  if row < 1 || row > 3
-      raise ArgumentError.new("Invalid num: please enter at least 1 piece.") if num < 1
-      raise ArgumentError.new("Invalid num: please enter no more than the number remaining in the row") if num > pieces_left(row)
+      raise ArgumentError.new("Invalid num: please enter at least 1 piece") if num < 1
+      raise ArgumentError.new("Invalid num: please enter no more than the number remaining in the row") if num > pieces_left_in_row(row)
+      raise ArgumentError.new("Invalid num: you may not take all remaining pieces") if num >= total_pieces_left
     end
 
-    def pieces_left(row)
+    def pieces_left_in_row(row)
       @position[row - 1].select { |e| e }.size
+    end
+
+    def total_pieces_left
+      @position.flatten.count
     end
 
     def update_board(row, num)
       new_row = []
-      (pieces_left(row) - num).times { new_row << true }
+      (pieces_left_in_row(row) - num).times { new_row << true }
       @position[row - 1] = new_row
-    end
-
-    def winner?
-      @position.flatten.count == 1
     end
 
   end
