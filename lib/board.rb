f6e042beaed765876e7a3c417f0952befa8e839f
Alex Brinkman <abrink@commercekitchen.com>
2018-11-19 22:59:26 -0700

    [CHG] Change to make the board more configurable
    I'm trying to figure the limits of computation for this naive algorithm
    but the board configurability didn't allow enough options to figure out
    where the algorithm broke down.  Now I have complete board configuration
    which shows that the computation breaks down at
    
    O O O O O
    O O O O O
    O O O O O
    
    (when the user takes 1 piece from row 1 for their first move)

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,6 @@
 module Game543
   class Board
 
-    # Assume the first row is FIRST_ROW_SIZE pieces, increasing 1 piece for each row.
-    FIRST_ROW_SIZE = 3
-    NUM_ROWS = 3
-
     def initialize(position = nil)
       @position = position || generate_board
     end
@@ -48,10 +44,10 @@ module Game543
     private
 
     def validate_move(row, num)
-      raise ArgumentError.new("Invalid row: please enter 1-#{NUM_ROWS}") if row < 1 || row > NUM_ROWS
-      raise ArgumentError.new("Invalid num: please enter at least 1 piece") if num < 1
-      raise ArgumentError.new("Invalid num: please enter no more than the number remaining in the row") if num > pieces_left_in_row(row)
-      raise ArgumentError.new("Invalid num: you may not take all remaining pieces") if num >= total_pieces_left
+      raise ArgumentError.new("Invalid row: please enter 1-#{@position.size}.") if row < 1 || row > @position.size
+      raise ArgumentError.new("Invalid number: please enter at least 1 piece.") if num < 1
+      raise ArgumentError.new("Invalid number: please enter no more than the number remaining in the row.") if num > pieces_left_in_row(row)
+      raise ArgumentError.new("Invalid number: you may not take all remaining pieces.") if num >= total_pieces_left
     end
 
     def pieces_left_in_row(row)
@@ -63,7 +59,7 @@ module Game543
     end
 
     def one_row_left?
-      NUM_ROWS.times do |row|
+      @position.size.times do |row|
         return true if pieces_left_in_row(row + 1) == total_pieces_left
       end
       false
@@ -82,11 +78,11 @@ module Game543
     end
 
     def generate_board
-      array = []
-      NUM_ROWS.times do |size|
-        array << Array.new(FIRST_ROW_SIZE + size) { true }
-      end
-      array
+      [
+        [true, true, true],
+        [true, true, true, true],
+        [true, true, true, true, true],
+      ]
     end
 
   end
